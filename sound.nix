8437817f62b4f9e6feca349d764b8cab658f8650
Norbiox <norbertchmiel.it@gmail.com>
2023-09-16 10:59:45 +0200

    switch to pipewire

--- sound.nix
+++ sound.nix
@@ -1,29 +1,53 @@
-{ pkgs, nixpkgs, home, ... }:
+{ pkgs, nixpkgs, ... }:
 
 {
   # Enable sound.
-  sound.enable = true;
-
-  # Enable pulseaudio
-  hardware.pulseaudio.enable = true;
-
-  # Enable compatibility with 32-bit applications
-  hardware.pulseaudio.support32Bit = true;
-
-  # Explicit PulseAudio support in applications
-  nixpkgs.config.pulseaudio = true;
-
-  # Enable extra codecs
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-
-  # Automatically switch audio to connected device
-  hardware.pulseaudio.extraConfig = ''
-    load-module module-switch-on-connect
-  '';
+  security.rtkit.enable = true;
+  services.pipewire = {
+		enable = true;
+    alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+		wireplumber.enable = true;
+#		media-session = {
+#			enable = true;
+#			config.bluez-monitor.rules = [
+#				{
+#					matches = [ { "device.name" = "~bluez.card.*"; } ];
+#					actions = {
+#						"update-props" = {
+#							"bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+#							"bluez5.msbc-support" = true;
+#							"bluez5.sbc-xq-support" = true;
+#						};
+#					};
+#				}
+# 				{
+#					matches = [
+#						{ "node.name" = "~bluez_input.*"; }
+#						{ "node.name" = "~bluez_output.*"; }
+#					];
+#				}
+#			];
+#		};
+  };
 
   # Install utilities
   environment.systemPackages = with pkgs; [
     pavucontrol
   ];
+
+  # Configure wireplumber
+  environment.etc = {
+  	"wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+			bluez_monitor.properties = {
+				["bluez5.enable-sbc-xq"] = true,
+				["bluez5.enable-msbc"] = true,
+				["bluez5.enable-hw-volume"] = true,
+				["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+			}
+	'';
+  };
 }
 
